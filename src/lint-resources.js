a56ca5665a71bb860729b383a414b75a7275f6fc
Brandon Hare <brandon.hare@blackbaud.me>
2019-07-30 15:38:20 -0400

    More progress

--- src/lint-resources.js
+++ src/lint-resources.js
@@ -1,21 +1,30 @@
 const glob = require('glob');
 const fs = require('fs-extra');
-const htmlResourcesRegex = /(?<=\{\{\s*?\n*?).*?(?=[\s|\n]+\|[\s|\n]+skyAppResources)/gm;
+const _flatten = require('lodash.flatten');
+const matchAll = require('string.prototype.matchall');
+const htmlResourcesRegex = /([^="\s{]+)\s+\|\s+skyAppResources/gm;
 const tsResourcesServiceLookupRegex = /\S*(?=:\s?SkyAppResourcesService)/gms;
 
 function lintResources(argv, config) {
-    const htmlFiles = glob.sync('./src/app/**/*component.html');
-    const tsFiles = glob.sync('./src/app/**/*component.ts')
-    const resourceFiles = glob.sync('./src/assets/locales/*.json');
-
-    const keys = getUniqueResourceStringKeys(resourceFiles);
-    const refs = getHtmlReferences(htmlFiles);
+    const htmlFilePaths = glob.sync('./src/app/**/*component.html');
+    const tsFilePaths = glob.sync('./src/app/**/!(*.spec).ts')
+    const resourceFilePaths = glob.sync('./src/assets/locales/*.json');
+    const keys = getResourceStringKeys(resourceFilePaths);
+    const htmlFiles = getFiles(htmlFilePaths);
+    const tsFiles = getFiles(tsFilePaths);
+    const unusedKeys = findUnusedKeysInResourceFiles(keys, htmlFiles.concat(tsFiles));
+    const htmlRefs = getHtmlReferences(htmlFiles);
     const tsRefs = getTSReferences(tsFiles);
-    console.log(tsRefs);
-
+    const missingKeys = findMissingKeysInResourceFiles(keys, htmlRefs.concat(tsRefs));
+    console.log('Missing Keys:');
+    console.table(missingKeys.missing);
+    console.log('Non Standard Keys:');
+    console.table(missingKeys.nonStandard);
+    console.log('Unused Keys:');
+    console.table(unusedKeys);
 }
 
-function getUniqueResourceStringKeys(filePaths) {
+function getResourceStringKeys(filePaths) {
     if (!filePaths) {
         return [];
     }
@@ -30,30 +39,48 @@ function getUniqueResourceStringKeys(filePaths) {
             return keys;
         } catch (error)
         {
-            console.error(`Unable to fetch resource file ${filePath}`);
+            console.error(`Unable to fetch resource file ${filePath}`, error);
             return keys;
         }
     }, []);
     return keys;
 }
 
-function getHtmlReferences(filePaths) {
+function getFiles(filePaths) {
     if (!filePaths) {
         return [];
     }
-    const references = filePaths.reduce((refs, filePath) => {
-        const htmlFileReferences = {
+    const files = filePaths.reduce((fileContents, filePath) => {
+        const file = {
             fileName: filePath.split('/').pop()
         };
         try {
             const fileString = fs.readFileSync(filePath, "utf8");
-            const resources = fileString.match(htmlResourcesRegex).map(r => r.replace(/'|\s|\\/gi, ''));
+            file.fileContents = fileString;
+        } catch (error) {
+            console.error(`Problem fetching file at path ${filePath}`);
+        }
+        fileContents.push(file);
+        return fileContents;
+    }, []);
+    return files;
+}
+
+function getHtmlReferences(files) {
+    if (!files) {
+        return [];
+    }
+    const references = files.reduce((refs, file) => {
+        const htmlFileReferences = {
+            fileName: file.fileName
+        };
+        try {
+            const resources = [...matchAll(file.fileContents,htmlResourcesRegex)].map(r => r.pop().replace(/'|\s|\\/gi, ''));
             htmlFileReferences.resources = resources;
             refs.push(htmlFileReferences);
             return refs;
         } catch (error) {
-            console.error(error);
-            // console.error(`Problem with html reference ${filePath}`);
+            console.error(`Problem with html reference ${file.fileName}`, error);
             refs.push(htmlFileReferences);
             return refs;
         }
@@ -61,35 +88,85 @@ function getHtmlReferences(filePaths) {
     return references;
 }
 
-function getTSReferences(filePaths) {
-    if (!filePaths) {
+function getTSReferences(files) {
+    if (!files) {
         return [];
     }
-    const references = filePaths.reduce((refs, filePath) => {
+    const references = files.reduce((refs, file) => {
         const tsFileReferences = {
-            fileName: filePath.split('/').pop()
+            fileName: file.fileName
         };
         try {
-            const fileString = fs.readFileSync(filePath, "utf8");
-            const serviceName = fileString.match(tsResourcesServiceLookupRegex);
+            const serviceName = file.fileContents.match(tsResourcesServiceLookupRegex);
             if (serviceName) {
                 const tsResourceStringLookupRegex = getTSResourcesStringLookupRegex(serviceName.shift());
-                const resources = fileString.match(tsResourceStringLookupRegex).map(r => r.replace(/'|\s|\\/gi, ''));
+                const resources = file.fileContents.match(tsResourceStringLookupRegex).map(r => {
+                    if (r.includes(',')) {
+                        r = r.split(',').shift();
+                    }
+                    return r.replace(/'|\s|\\/gi, '');
+                });
                 tsFileReferences.resources = resources;
                 refs.push(tsFileReferences);
             }
             return refs;
 
         } catch (error) {
-            console.error(`Problem with ts reference ${filePath}`);
+            console.error(`Problem with ts reference ${file.fileName}`, error);
             refs.push(tsFileReferences);
             return refs;
         }
     }, []);
+    return references;
 }
 
 function getTSResourcesStringLookupRegex(serviceName) {
     return new RegExp(`(?<=${serviceName}\\.getString\\()(.*?)(?=\\))`, 'gms');
+}
+
+function findUnusedKeysInResourceFiles(resourceFiles, files) {
+    const results = resourceFiles.reduce((unusedKeys, file) => {
+        file.keys.map(key => {
+            const found = files.some(f => f.fileContents.includes(key));
+            if (!found) {
+                unusedKeys.push({
+                    resourceFileName: file.resourceFileName,
+                    key
+                });
+            }
+        });
+        return unusedKeys;
+    }, []);
+    return results;
+}
+
+function findMissingKeysInResourceFiles(resourceFiles, references) {
+    const results = resourceFiles.reduce((missingKeys, resourceFile) => {
+        references.map(referenceFile => {
+            const fileName = referenceFile.fileName;
+            referenceFile.resources.map(key => {
+                const isMissing = !resourceFile.keys.includes(key);
+                const isRawKey = key.match(/[^a-zA-Z0-9_]/g);
+                if (isMissing) {
+                    const result = {
+                        resourceFileName: resourceFile.resourceFileName,
+                        fileName,
+                        key
+                    };
+                    if (isRawKey) {
+                        missingKeys.missing.push(result);
+                    } else {
+                        missingKeys.nonStandard.push(result);
+                    }
+                }
+            });
+        });
+        return missingKeys;
+    }, {
+        missing: [],
+        nonStandard: []
+    });
+    return results;
 }
 
 module.exports = lintResources;
